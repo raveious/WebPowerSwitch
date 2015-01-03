515a291fc246e9584e2382abe44c0fbd3679db0c
Ian Wakely <raveious.irw@gmail.com>
2015-01-02 21:47:12 -0500

    Adding in a cli using click to make the module runnable from the command line

---
+++ WPS/__main__.py
@@ -0,0 +1,56 @@
+import click
+from WebPowerSwitch import WebPowerSwitch
+
+def _action(args):
+    power = WebPowerSwitch(args.get('hostname'), args.get('port'),
+                           args.get('username'), args.get('password'))
+
+    if args.get('action') is 'on':
+        power.on(args.get('outlet'))
+    if args.get('action') is 'off':
+        power.off(args.get('outlet'))
+    if args.get('action') is 'cycle':
+        power.cycle(args.get('outlet'), args.get('delay'))
+
+@click.group()
+@click.option('-h', '--hostname', default='192.168.0.100', help='IP or hostname of the Web Power Switch')
+@click.option('-o', '--port', default=80, help='Port the web interface can be found on')
+@click.option('-u', '--username', default='admin', help='Username to control the web power switch as')
+# @click.password_option('-p', '--password', default='1234', help='Password for user login')
+@click.option('-p', '--password', default='1234', help='Password for user login')
+@click.pass_obj
+def cli(clx, hostname, port, username, password):
+    ctx['hostname'] = hostname
+    ctx['port'] = port
+    ctx['username'] = username
+    ctx['password'] = password
+
+@cli.command()
+@click.argument('outlet', type=int)
+@click.pass_obj
+def on(ctx, outlet):
+    ctx['action'] = 'on'
+    ctx['outlet'] = outlet
+    _action(ctx)
+
+@cli.command()
+@click.argument('outlet', type=int)
+@click.pass_obj
+def off(ctx, outlet):
+    ctx['action'] = 'off'
+    ctx['outlet'] = outlet
+    _action(ctx)
+
+@cli.command()
+@click.argument('outlet', type=int)
+@click.option('-d', '--delay', default=None, type=int, help='Durration of power cycle')
+@click.pass_obj
+def cycle(ctx, outlet, delay):
+    ctx['action'] = 'cycle'
+    ctx['outlet'] = outlet
+    ctx['delay'] = delay    
+    _action(ctx)
+
+if __name__ == '__main__':
+    ctx = {}
+    cli(obj=ctx)
